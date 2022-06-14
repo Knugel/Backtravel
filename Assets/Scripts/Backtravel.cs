2b6e678f68bdd8b78e3f1d14839883f028282025
Florian Hofmair <fhofmair@gmail.com>
2022-06-14 20:28:00 +0200

    Initial commit

---
+++ Assets/Scripts/Backtravel.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Overmine.API;
+using UnityEngine;
+
+public class Backtravel : Mod
+{
+    public const string HarmonyInstanceID = "com.knugel.backtravel";
+    
+    public Backtravel(ModResources resources) : base(resources)
+    {
+        Patch<WarpPopupPatches>();
+    }
+    
+    private static void Patch<T>()
+    {
+        Debug.Log($"Patch: {typeof(T).Name}");
+        Harmony.CreateAndPatchAll(typeof(T), HarmonyInstanceID);
+    }
+}
---
+++ Assets/Scripts/WarpPopupPatches.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Thor;
+
+public class WarpPopupPatches
+{
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(WarpPopup), "Initialize")]
+    public static IEnumerable<CodeInstruction> OnWarpPopup(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+    {
+        var matcher = new CodeMatcher(instructions, il)
+            .MatchForward(true, new CodeMatch(i => i.opcode == OpCodes.Brfalse));
+            
+        // Remove the condition that the map zone has to be later then the current one
+        return matcher.RemoveInstructions(13).InstructionEnumeration();
+    }
+}
